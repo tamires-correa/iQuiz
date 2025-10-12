4ac189373e658ac0a2ebcf34d5b0bade9b3bafd3
Tamires Correa <tamirescorrea.dev@gmail.com>
2025-10-12 15:06:13 -0300

    cria tela sobre obras, conecta botão para tela e cria class works

---
+++ iQuiz/Model/Works.swift
@@ -0,0 +1,27 @@
+//
+//  Works.swift
+//  iQuiz
+//
+//  Created by Tamires Corrêa on 12/10/25.
+//
+
+import Foundation
+
+struct Works{
+    var name: String
+    var imageName: String
+    var year: Int
+}
+
+let works: [Works] = [
+    Works(name: "Catedral Metropolitana de Brasília", imageName: "CatedralBrasília", year: 1970),
+    Works(name: "MON (Museu Oscar Niemeyer)", imageName: "olho", year: 2002),
+    Works(name: "Copan", imageName: "Copan", year: 1966),
+    Works(name: "Congresso Nacional", imageName: "congresso", year: 1960),
+    Works(name: "MAC (Museu de Arte Contemporânea)", imageName: "mac", year: 1996),
+    Works(name: "Palácio do Planalto", imageName: "palacio planalto", year: 1960),
+    Works(name: "Igreja de Pampulha", imageName: "pampulha", year: 1943),
+    Works(name: "Palácio da Alvorada", imageName: "alvorada", year: 1958),
+    Works(name: "Teatro Popular", imageName: "caminho", year: 2007),
+    Works(name: "Supremo Tribunal de Justiça", imageName: "sfj", year: 1960)
+    ]
---
+++ iQuiz/Views/AboutWorksViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutWorksViewController.swift
+//  iQuiz
+//
+//  Created by Tamires Corrêa on 12/10/25.
+//
+
+import UIKit
+
+class AboutWorksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
